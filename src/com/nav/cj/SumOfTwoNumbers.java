35bb56a8f3c96a49194363e38a4bc28e6ab09cc6
akulanaveen <61976868+akulanaveen@users.noreply.github.com>
2020-04-22 22:10:41 -0400

    Added new program to sum of two numbers

---
+++ src/com/nav/cj/SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.nav.cj;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("Result is :"+c);
+	}
+}
