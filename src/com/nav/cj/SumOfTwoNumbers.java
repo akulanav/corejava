cab4f686d40691cc17f36a6c6084751340feb168
akulanav <64173722+akulanav@users.noreply.github.com>
2020-04-22 22:13:35 -0400

    updated print statement
    Detailed message is added

--- src/com/nav/cj/SumOfTwoNumbers.java
+++ src/com/nav/cj/SumOfTwoNumbers.java
@@ -6,6 +6,6 @@ public class SumOfTwoNumbers {
 		int a = 10;
 		int b = 20;
 		int c = a + b;
-		System.out.println("Result is :"+c);
+		System.out.println("The sum of "+a +"and "+b +" is :"+c);
 	}
 }
